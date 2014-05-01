76dddcdc97b2d5dba29d1669b753fc9f2528ee1d
catarinaICS <catarina.santana@gmail.com>
2014-05-01 18:22:46 +0100

    Use of IdentityHashMap

--- src/ist/meic/pa/Trace.java
+++ src/ist/meic/pa/Trace.java
@@ -1,34 +1,32 @@
 package ist.meic.pa;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.IdentityHashMap;
 import java.util.Map;
 
 public class Trace {
 	
-	private static Map<ObjectWrapper, ArrayList<TraceInfo>> history = new HashMap<ObjectWrapper, ArrayList<TraceInfo>>();
+	private static Map<Object, ArrayList<TraceInfo>> history = new IdentityHashMap<Object, ArrayList<TraceInfo>>();
 	
 	@Untraceable
 	public static void addInfo(Object object, String direction, String method, String filename, int lineNumber) {
 		TraceInfo info = new TraceInfo(direction, method, filename, lineNumber);
-		ObjectWrapper obj = new ObjectWrapper(object);
-		if (history.containsKey(obj)) {
-			history.get(obj).add(info);
+		if (history.containsKey(object)) {
+			history.get(object).add(info);
 		} else {
 			ArrayList<TraceInfo> lst = new ArrayList<TraceInfo>();
 			lst.add(info);
-			history.put(obj, lst);
+			history.put(object, lst);
 		}
 	}
 
 	@Untraceable
 	public static void print(Object object) {
-		ObjectWrapper obj = new ObjectWrapper(object);
-		if (!history.containsKey(obj)) {
+		if (!history.containsKey(object)) {
 			System.err.println("Tracing for " + object + " is nonexistent!");
 		} else {
 			System.err.println("Tracing for " + object);
-			ArrayList<TraceInfo> info = history.get(obj);
+			ArrayList<TraceInfo> info = history.get(object);
 			
 			for (TraceInfo i : info) {
 				System.err.println("  " + i);
